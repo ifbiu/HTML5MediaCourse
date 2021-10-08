bb580908b4eb5d2fba3c4e58400ad92ae2e4a4b1
candide <biuvip@163.com>
2021-10-08 22:19:09 +0800

    使用拍照

--- UsingCamera/app.js
+++ UsingCamera/app.js
@@ -1,6 +1,18 @@
-(async function(){
-    document.querySelector("video").srcObject = await navigator.mediaDevices.getUserMedia({
-        audio:false,
-        video:true
-    });
-})();
+new Vue({
+    el:"#app",
+    mounted(){
+        this.initApp();
+    },
+    methods:{
+        async initApp(){
+            this.$refs.video.srcObject = await navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false
+            });
+            this._context2d = this.$refs.canvas.getContext("2d");
+        },
+        btnTakePhotoClicked(){
+            this._context2d.drawImage(this.$refs.video,0,0,400,300);
+        }
+    }
+})
